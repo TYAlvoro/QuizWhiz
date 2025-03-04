bdec6d89d6b1e1d4015158229fda5c36e52defaa
Тамирлан Яхьяев <alvoro@MacBook---AlVoro.local>
2025-03-04 20:58:16 +0300

    Добавлен и настроен в базовой структуре основной сервис - создание квизов

---
+++ quiz-service/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("org.springframework.boot") version "3.1.1"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.0"
+}
+
+group = "com.quizwhiz"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // DevTools (опционально)
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ quiz-service/src/main/kotlin/com/quizwhiz/quizservice/QuizServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.quizwhiz.quizservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuizServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<QuizServiceApplication>(*args)
+}
