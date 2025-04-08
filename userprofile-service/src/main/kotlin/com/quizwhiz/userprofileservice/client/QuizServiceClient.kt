1b795dfb00853cbb603b8bfa9f30f867d616e6a6
Шустрова Юлия <alvoro@MacBook---AlVoro.local>
2025-04-08 22:18:26 +0300

    Updated QuizClient

--- userprofile-service/src/main/kotlin/com/quizwhiz/userprofileservice/client/QuizServiceClient.kt
+++ userprofile-service/src/main/kotlin/com/quizwhiz/userprofileservice/client/QuizServiceClient.kt
@@ -2,15 +2,20 @@ package com.quizwhiz.userprofileservice.client
 
 import com.quizwhiz.userprofileservice.dto.CourseDto
 import com.quizwhiz.userprofileservice.dto.RecentQuizAttemptDto
+import com.quizwhiz.userprofileservice.dto.AchievementDto
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
 @FeignClient(name = "quiz-service", url = "\${quizservice.url}")
 interface QuizServiceClient {
+
     @GetMapping("/internal/courses/by-teacher/{teacherId}")
     fun getCoursesByTeacherId(@PathVariable teacherId: Long): List<CourseDto>
 
     @GetMapping("/internal/quiz-attempts/recent/{nickname}")
     fun getRecentQuizAttempts(@PathVariable nickname: String): List<RecentQuizAttemptDto>
+
+    @GetMapping("/internal/achievements/{nickname}")
+    fun getAchievements(@PathVariable nickname: String): List<AchievementDto>
 }
