a0c9e14ec48f45e6d15de445f79aa2160902d007
Шустрова Юлия <alvoro@MacBook---AlVoro.local>
2025-04-08 23:25:43 +0300

    Added tests for AuthService

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -30,10 +30,17 @@ dependencies {
     implementation("org.webjars:bootstrap:5.2.3")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("jakarta.servlet:jakarta.servlet-api:6.0.0")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.mockito:mockito-core:5.3.1")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
+    implementation("jakarta.servlet:jakarta.servlet-api:6.0.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
