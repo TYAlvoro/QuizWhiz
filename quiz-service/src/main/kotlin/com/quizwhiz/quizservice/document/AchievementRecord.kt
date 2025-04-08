c373ed452414b65eb88247032022634c3d04329a
Шустрова Юлия <alvoro@MacBook---AlVoro.local>
2025-04-08 22:15:25 +0300

    Designed achievement doc

--- quiz-service/src/main/kotlin/com/quizwhiz/quizservice/document/AchievementRecord.kt
+++ quiz-service/src/main/kotlin/com/quizwhiz/quizservice/document/AchievementRecord.kt
@@ -1,4 +1,14 @@
 package com.quizwhiz.quizservice.document
 
-class AchievementRecord {
-}
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.util.Date
+
+@Document(collection = "achievements")
+data class AchievementRecord(
+    @Id val id: String? = null,
+    val nickname: String,
+    val achievementName: String,
+    val description: String,
+    val awardedAt: Date = Date()
+)
